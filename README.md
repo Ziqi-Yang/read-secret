82d4303897bdb0dea3cee18fc9f2f40c68983429
Ziqi Yang <mr.ziqiyang@gmail.com>
2023-06-27 10:42:30 +0800

    style: format code

--- README.md
+++ README.md
@@ -19,28 +19,26 @@ read-secret = "0.1"
 
 This example code is in file `examples/e1.es`, and you can run it by executing `cargo run --example e1`
 ``` rust
+use read_secret::{DecryptMethod, SecretType};
 use std::process::Command;
-use read_secret::{
-		DecryptMethod, SecretType
-};
 
 fn main() {
-		// read secret from environment variable.
-		let mut dm = DecryptMethod::None;
+    // read secret from environment variable.
+    let mut dm = DecryptMethod::None;
     let st = SecretType::Env("XDG_SESSION_TYPE".to_string());
     let sr = read_secret::read_secret(st, &mut dm).unwrap();
     assert_eq!("wayland", sr);
 
-		// read secret from file and decrypt it using gnupg.
-		let mut dm = DecryptMethod::GPG;
+    // read secret from file and decrypt it using gnupg.
+    let mut dm = DecryptMethod::GPG;
     let st = SecretType::File("examples/pass_gpg.asc".to_string());
     let sr = read_secret::read_secret(st, &mut dm).unwrap();
     assert_eq!("El Psy Kongaroo", sr);
 
-		// store encrypted secret inside string and decrypt it using custom command.
-		let mut temp = Command::new("wc"); // avoid E0716 -- temporary value is being dropped
-		let custom_command = temp.args(["-c"]);
-		let mut dm = DecryptMethod::Custom(custom_command);
+    // store encrypted secret inside string and decrypt it using custom command.
+    let mut temp = Command::new("wc"); // avoid E0716 -- temporary value is being dropped
+    let custom_command = temp.args(["-c"]);
+    let mut dm = DecryptMethod::Custom(custom_command);
     let st = SecretType::String("El Psy Kongaroo".to_string());
     let sr = read_secret::read_secret(st, &mut dm).unwrap();
     assert_eq!("15\n", sr);
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ use std::{
     fs::File,
     io::{self, Read, Write},
     path::Path,
-		process::{Command, Stdio, Child},
+    process::{Child, Command, Stdio},
 };
 
 /// Type of Secret:
@@ -32,18 +32,11 @@ pub enum DecryptMethod<'a> {
 /// Provide an common entry to read secret
 pub fn read_secret(stype: SecretType, dm: &mut DecryptMethod) -> Result<String, Box<dyn Error>> {
     let estr = match stype {
-        SecretType::Env(name) => {
-						read_env(&name)
-								.map_err(|e| Box::new(e) as Box<dyn Error>)?
-				},
-				SecretType::File(path) => {
-						read_file(&path)
-								.map_err(|e| Box::new(e) as Box<dyn Error>)?
-				},
+        SecretType::Env(name) => read_env(&name).map_err(|e| Box::new(e) as Box<dyn Error>)?,
+        SecretType::File(path) => read_file(&path).map_err(|e| Box::new(e) as Box<dyn Error>)?,
         SecretType::String(secret) => secret,
     };
-		decrypt(estr, dm)
-				.map_err(|e| Box::new(e) as Box<dyn Error>)
+    decrypt(estr, dm).map_err(|e| Box::new(e) as Box<dyn Error>)
 }
 
 /// Get a value of the given environment variable. If the given environment variable doesn't exist,
@@ -62,38 +55,38 @@ pub fn read_file(path: &str) -> io::Result<String> {
 
 /// Decrypt a encoded password using provided method
 fn decrypt(estr: String, dm: &mut DecryptMethod) -> io::Result<String> {
-		match dm {
-				DecryptMethod::None => Ok(estr),
-				DecryptMethod::GPG => {
-						let gpg = Command::new("gpg")
-								.args(["--no-tty", "-q", "-d", "-a"])
-								.stdin(Stdio::piped())
-								.stdout(Stdio::piped())
-								.spawn()
-								.expect("Failed to spawn `gpg`: please ensure you have it installed.");
-						let res = get_command_output(gpg, estr)?;
-						// remove the tailing new line character
-						let res = res.chars().take(res.len() - 1).collect();
-						Ok(res)
-				},
-				DecryptMethod::Custom(command) => {
-						let command = command
-								.stdin(Stdio::piped())
-								.stdout(Stdio::piped())
-								.spawn().expect("Failed to spawn command");
-						get_command_output(command, estr)
-				},
-		}
+    match dm {
+        DecryptMethod::None => Ok(estr),
+        DecryptMethod::GPG => {
+            let gpg = Command::new("gpg")
+                .args(["--no-tty", "-q", "-d", "-a"])
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to spawn `gpg`: please ensure you have it installed.");
+            let res = get_command_output(gpg, estr)?;
+            // remove the tailing new line character
+            let res = res.chars().take(res.len() - 1).collect();
+            Ok(res)
+        }
+        DecryptMethod::Custom(command) => {
+            let command = command
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to spawn command");
+            get_command_output(command, estr)
+        }
+    }
 }
 
 fn get_command_output(mut command: Child, input: String) -> io::Result<String> {
-		let mut stdin = (&mut command).stdin.take().expect("Failed to take stdin");
-		stdin.write_all(input.as_bytes())?;
-		drop(stdin);
-		let output = command.wait_with_output()?;
-		let res = String::from_utf8(output.stdout)
-				.expect("Cannot convert utf8 bytes into String.");
-		Ok(res)
+    let mut stdin = (&mut command).stdin.take().expect("Failed to take stdin");
+    stdin.write_all(input.as_bytes())?;
+    drop(stdin);
+    let output = command.wait_with_output()?;
+    let res = String::from_utf8(output.stdout).expect("Cannot convert utf8 bytes into String.");
+    Ok(res)
 }
 
 #[cfg(test)]
@@ -103,7 +96,7 @@ mod tests {
 
     #[test]
     fn test_read_secret() -> Result<(), Box<dyn Error>> {
-				let mut dm = DecryptMethod::None;
+        let mut dm = DecryptMethod::None;
         let st = SecretType::Env("CARGO_PKG_VERSION".to_string());
         let sr = read_secret(st, &mut dm)?;
         assert_eq!(LIB_VERSION, sr);
@@ -131,18 +124,18 @@ mod tests {
         Ok(())
     }
 
-		#[test]
-		fn test_decrypt() -> io::Result<()> {
-				let sl = "El Psy Kongaroo";
-				// DecryptMethod::None
-				let mut dm = DecryptMethod::None;
-				let sr = sl;
-				let sr = decrypt(sr.to_string(), &mut dm)?;
-				assert_eq!(sr, sl);
-				
-				// DecryptMethod::GPG
-				let mut dm = DecryptMethod::GPG;
-				let sr = "-----BEGIN PGP MESSAGE-----
+    #[test]
+    fn test_decrypt() -> io::Result<()> {
+        let sl = "El Psy Kongaroo";
+        // DecryptMethod::None
+        let mut dm = DecryptMethod::None;
+        let sr = sl;
+        let sr = decrypt(sr.to_string(), &mut dm)?;
+        assert_eq!(sr, sl);
+
+        // DecryptMethod::GPG
+        let mut dm = DecryptMethod::GPG;
+        let sr = "-----BEGIN PGP MESSAGE-----
 
 hQGMAw4MNp4TmOFvAQwAuXN8xO+ca+Bz8bEFqnEB8cuxKYd0rCLa7UqN446DLnbj
 0g5IqyfhCgzNgbMN9LN3pYALwPrNEw6bSK6QoOn3ZtCOQKRSjH1WprRGUx3Fc+dO
@@ -156,17 +149,16 @@ fmvb8an9JMgXwJDb0EHO0ksB6CaAfueLAR4sL8OpmUbeVg/kJv9fgvkHXvMMnFp+
 BafOEV3SWmeMynyfYk2g12wtph+Jm9EDq3PLokHAlfp0EYRqTSF0VDaHYdw=
 =LM7a
 -----END PGP MESSAGE-----";
-				let sr = decrypt(sr.to_string(), &mut dm)?;
-				assert_eq!(sl, sr);
-				
-				// DecryptMethod::Custom
-				let mut binding = Command::new("wc");
-				let custom_command = binding.args(["-c"]);
-				let mut dm = DecryptMethod::Custom(custom_command);
-				let sr = sl;
-				let sr = decrypt(sr.to_string(), &mut dm)?;
-				assert_eq!("15\n", sr);
-				Ok(())
-		}
-}
+        let sr = decrypt(sr.to_string(), &mut dm)?;
+        assert_eq!(sl, sr);
 
+        // DecryptMethod::Custom
+        let mut binding = Command::new("wc");
+        let custom_command = binding.args(["-c"]);
+        let mut dm = DecryptMethod::Custom(custom_command);
+        let sr = sl;
+        let sr = decrypt(sr.to_string(), &mut dm)?;
+        assert_eq!("15\n", sr);
+        Ok(())
+    }
+}
